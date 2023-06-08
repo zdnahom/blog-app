e8a40def07ffcdfbda4187a87c51742a8819cd2f
Nahom <zerihunnahom@gmail.com>
2023-06-08 19:59:06 +0300

    Fix linters

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-    #tests go here
-    user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
-    post = Post.create(author: user, title: 'Hello', text: 'This is my first post')
+  # tests go here
+  user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+  post = Post.create(author: user, title: 'Hello', text: 'This is my first post')
 
-    subject{Comment.create(post: post, author: user, text: 'Hi Tom!' )}
+  subject { Comment.create(post:, author: user, text: 'Hi Tom!') }
 
-    before{ subject.save }
+  before { subject.save }
 
-    describe '#update_comments_counter' do
-        it 'should increment the comments_counter of a given post to 1' do
-            subject.update_comments_counter
+  describe '#update_comments_counter' do
+    it 'should increment the comments_counter of a given post to 1' do
+      subject.update_comments_counter
 
-            expect(subject.post.comments_counter).to eq 1
-        end
+      expect(subject.post.comments_counter).to eq 1
     end
   end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,38 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    #tests go here
-    subject{User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')}
+  # tests go here
+  subject { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
 
-    before{ subject.save }
+  before { subject.save }
 
-    describe 'Validations' do
-        it 'name should be present' do
-            subject.name = ''
-            expect(subject).to_not be_valid
-        end
-
-        it 'posts_counter should be an integer' do
-            subject.posts_counter = 1.4
-            expect(subject).to_not be_valid
-        end
-
-        it 'posts_counter should be greater than or equal to zero' do
-            subject.posts_counter = -1
-            expect(subject).to_not be_valid
-        end
+  describe 'Validations' do
+    it 'name should be present' do
+      subject.name = ''
+      expect(subject).to_not be_valid
     end
 
-    describe '#recent_posts' do
-        it 'should return 3 recent posts' do
-            first_post = Post.create(author: subject, title: 'Hello', text: 'This is my first post')
-            second_post = Post.create(author: subject, title: 'Hello2', text: 'This is my 2nd post')
-            third_post = Post.create(author: subject, title: 'Hello3', text: 'This is my 3rd post')
-            forth_post = Post.create(author: subject, title: 'Hello4', text: 'This is my 4th post')
-            fifth_post = Post.create(author: subject, title: 'Hello5', text: 'This is my 5th post')
+    it 'posts_counter should be an integer' do
+      subject.posts_counter = 1.4
+      expect(subject).to_not be_valid
+    end
 
-            expect(subject.recent_posts.length).to eq 3
-            expect(subject.recent_posts[0].title).to eq 'Hello5'
-        end
+    it 'posts_counter should be greater than or equal to zero' do
+      subject.posts_counter = -1
+      expect(subject).to_not be_valid
     end
   end
+
+  describe '#recent_posts' do
+    it 'should return 3 recent posts' do
+      Post.create(author: subject, title: 'Hello', text: 'This is my first post')
+      Post.create(author: subject, title: 'Hello2', text: 'This is my 2nd post')
+      Post.create(author: subject, title: 'Hello3', text: 'This is my 3rd post')
+      Post.create(author: subject, title: 'Hello4', text: 'This is my 4th post')
+      Post.create(author: subject, title: 'Hello5', text: 'This is my 5th post')
+
+      expect(subject.recent_posts.length).to eq 3
+      expect(subject.recent_posts[0].title).to eq 'Hello5'
+    end
+  end
+end
