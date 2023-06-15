ef9f6f8cbef299c5510525d158a6cdb917750313
Nahom <zerihunnahom@gmail.com>
2023-06-15 23:07:42 +0300

    Add new and create controller action for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,20 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @author = User.find(params[:user_id])
   end
+
+  def new; end
+
+  def create 
+    title = params[:title]
+    text = params[:text]
+    post = Post.new(author:current_user, title:title, text:text)
+  
+    if post.save
+      post.update_posts_counter
+      redirect_to user_posts_path, notice: "Post created successfully!!"
+    else
+      flash.now[:alert] = "Unable to create a post"
+      render :new
+    end
+  end
 end
