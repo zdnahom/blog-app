148d8ed5333d35174cbc80868d35ab6fa6e83be6
Nahom <zerihunnahom@gmail.com>
2023-06-16 00:00:56 +0300

    Add new and create controller action for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,17 @@
 class CommentsController < ApplicationController
-  def index; end
+  def new; end
 
-  def show; end
+  def create 
+    text = params[:text]
+    post = Post.find(params[:post_id])
+    comment = Comment.new(post:post, author:current_user, text:text)
+  
+    if comment.save
+      comment.update_comments_counter
+      redirect_to user_post_path(id: post.id), notice: "Comment added successfully!!"
+    else
+      flash.now[:alert] = "Unable to add a comment"
+      render :new
+    end
+  end
 end
