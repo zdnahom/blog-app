8666232f476eb5da8874aecf97bd0aa87b472822
Nahom <zerihunnahom@gmail.com>
2023-06-16 11:27:18 +0300

    Add create action for likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,8 @@
 class LikesController < ApplicationController
-  def index; end
-
-  def show; end
+  def create
+    post = Post.find(params[:post_id])
+    like = Like.create(post: post, author: current_user)
+    like.update_likes_counter
+    redirect_to user_post_path(id: post.id)
+  end
 end
