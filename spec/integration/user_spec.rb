6417a365e373fc283c8474594b3a679aaeaf4929
isaacg <iwanyoike65@gmail.com>
2023-06-19 20:05:29 +0300

    Add user show page tests

--- spec/integration/user_spec.rb
+++ spec/integration/user_spec.rb
@@ -28,4 +28,50 @@ RSpec.describe 'User page', type: :system do
       expect(page).to have_content(user.name)
     end
   end
+
+  describe 'User show page' do
+    it 'shows user profile picture' do
+      visit user_path(user)
+      expect(page).to have_css('img[alt="user photo"]')
+    end
+
+    it 'shows user name' do
+      visit user_path(user)
+      expect(page).to have_content(user.name)
+    end
+
+    it 'shows post counts' do
+      visit user_path(user)
+      expect(page).to have_content(1)
+    end
+
+    it 'shows user bio' do
+      visit user_path(user)
+      expect(page).to have_content(user.bio)
+    end
+
+    it 'shows user posts' do
+      visit user_path(user)
+      expect(page).to have_content(1)
+      expect(page).to have_content('This is a post')
+    end
+
+    it 'shows button to see all user posts' do
+      visit user_path(user)
+      expect(page).to have_link('see all posts')
+    end
+
+    it 'user clicks on post name and goes to post page' do
+      visit user_path(user)
+      click_on 'Post#1'
+      expect(page).to have_content('This is a post')
+    end
+
+    it 'user clicks on see all posts button and goes to user posts page' do
+      visit user_path(user)
+      click_on 'see all posts'
+      expect(page).to have_content(user.name)
+      expect(page).to have_content('This is a post')
+    end
+  end
 end
