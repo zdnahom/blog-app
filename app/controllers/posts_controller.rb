413c2b49e54ba7c3577efced91b9518b88fa3721
Nahom <zerihunnahom@gmail.com>
2023-06-19 17:25:15 +0300

    Solve n+1 problem of posts and comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,14 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.where(author_id: params[:user_id])
-    @author = User.find(params[:user_id])
+    def index
+      @author = User.find(params[:user_id])
+      @posts = @author.posts.includes(:comments)
   end
 
   def show
-    @post = Post.find(params[:id])
     @author = User.find(params[:user_id])
+    @post = Post.find(params[:id])
   end
-
+  
   def new
     @post = Post.new
   end
