d81b3d4572063b58f2cfe7ae74ff8a30e07d57c0
Nahom <zerihunnahom@gmail.com>
2023-06-22 14:23:59 +0300

    Fix update_posts_counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,19 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
-  has_many :likes
-  has_many :comments
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  after_save :update_posts_counter
+  after_destroy :update_posts_counter
 
   validates :title, presence: true, length: { in: 1..250 }
   validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def update_posts_counter
-    posts_counter = author.posts_counter || 0
-    author.update(posts_counter: posts_counter + 1)
+    author.update(posts_counter: author.posts.count)
   end
 
   def recent_comments
     comments.includes(:author).order(created_at: :desc).limit(5)
   end
+  
 end
