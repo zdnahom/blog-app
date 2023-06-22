22145f4f0a0af1b237d143ec9d89720b6c40d452
Nahom <zerihunnahom@gmail.com>
2023-06-22 14:25:41 +0300

    Add authorization for posts

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,10 +6,13 @@ class Ability
   def initialize(user)
     # Define abilities for the user here. For example:
     #
-    #   return unless user.present?
-    #   can :read, :all
-    #   return unless user.admin?
-    #   can :manage, :all
+      user ||= User.new
+      can :read, :all
+      return unless user.present?
+      can :destroy, Post, author_id: user.id
+      can :create, Post, author_id: user.id
+      return unless user.admin?
+      can :manage, :all
     #
     # The first argument to `can` is the action you are giving the user
     # permission to do.
