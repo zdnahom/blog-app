15a1d1b0b760b9938d9b97b38aebfa008de1bcf5
isaacg <iwanyoike65@gmail.com>
2023-06-23 19:04:04 +0300

    Add generate auth_key method in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,20 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
 
+  before_create :generate_auth_key
+  
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
+
+  def generate_auth_key
+  begin
+    self.auth_key = Devise.friendly_token
+  end while self.class.exists?(auth_key: auth_key)
+end
 
   def admin?
     role == 'admin'
