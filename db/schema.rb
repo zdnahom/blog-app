4692252b3a53aef19e8af471a5413477696d6358
isaacg <iwanyoike65@gmail.com>
2023-06-23 19:03:15 +0300

    Add auth_key column in database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_22_085413) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_23_160213) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_22_085413) do
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
     t.string "role"
+    t.string "auth_key"
+    t.index ["auth_key"], name: "index_users_on_auth_key", unique: true
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
