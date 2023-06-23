d58bf3ce9af7d0f05a451711d4d5a22fea4be64d
Nahom <zerihunnahom@gmail.com>
2023-06-23 18:32:58 +0300

    Add api routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,13 @@ Rails.application.routes.draw do
       end
     end
   end
+  
+  root "users#index"
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do 
+      resources :comments, only: [:new, :create, :destroy]
+      resources :likes, only: [:create]
+    end
+  end
 
 end
