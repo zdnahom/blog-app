329d71d99b99d0e3fe5cc95a20a38799edafdd9e
Nahom <zerihunnahom@gmail.com>
2023-06-23 18:30:52 +0300

    Update config files

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  devise_for :users
-  # Defines the root path route ("/")
-  root "users#index"
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create, :destroy] do 
-      resources :comments, only: [:new, :create, :destroy]
-      resources :likes, only: [:create]
+  # devise_for :users
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :users, only: [:index, :show] do
+        resources :posts, only: [:index, :show, :create, :destroy] do 
+          resources :comments, only: [:create, :destroy]
+          resources :likes, only: [:create]
+        end
+      end
     end
   end
 
